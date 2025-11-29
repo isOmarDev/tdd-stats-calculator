e5d6438101ec3a875ba5939d8515f2b395b05075
omar kamel <isomardev@gmail.com>
2025-11-30 01:45:55 +0200

    refactor: improve calculation performance

--- src/stats-calculator.ts
+++ src/stats-calculator.ts
@@ -1,44 +1,33 @@
 export class StatsCalculator {
-  private static isEmpty(sequence: (number | undefined)[]) {
-    return (
-      sequence.every((value) => value === undefined) ||
-      sequence.length === 0
-    );
-  }
-
   static run(sequence: (number | undefined)[]) {
-    if (this.isEmpty(sequence)) {
+    let min: number | undefined = undefined;
+    let max: number | undefined = undefined;
+    let sum = 0;
+    let count = 0;
+    let avg = 0;
+
+    for (const value of sequence) {
+      if (value === undefined) continue;
+
+      if (min === undefined || value < min) min = value;
+      if (max === undefined || value > max) max = value;
+
+      sum += value;
+      count++;
+      avg = sum / count;
+    }
+
+    if (count === 0) {
       throw new Error('Cannot calculate stats for empty array');
     }
 
-    const minValue = sequence.reduce((prevValue, currentValue) =>
-      currentValue < prevValue ? currentValue : prevValue
-    );
-
-    const maxValue = sequence.reduce((prevValue, currentValue) =>
-      currentValue > prevValue ? currentValue : prevValue
-    );
-
-    const totalElements = sequence.reduce(
-      (prevValue: number, currentValue) =>
-        currentValue !== undefined ? ++prevValue : prevValue,
-      0
-    );
-
-    const avgValue =
-      sequence.reduce(
-        (prevValue: number, currentValue) =>
-          currentValue === undefined
-            ? prevValue
-            : currentValue + prevValue,
-        0
-      ) / totalElements;
-
     return {
-      min: minValue,
-      max: maxValue,
-      count: totalElements,
-      avg: avgValue,
+      min,
+      max,
+      count,
+      avg,
     };
   }
 }
+
+console.log(137 / 7);
